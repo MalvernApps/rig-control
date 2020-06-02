fd74d9e01774751969d4fdb0753106b055e127a4
grmurray <grmurray@qinetiq.com>
2020-06-02 12:23:27 +0100

    Working and adding new font.

--- serialtest/MainWindow.xaml.cs
+++ serialtest/MainWindow.xaml.cs
@@ -27,6 +27,41 @@ namespace serialtest
         public MainWindow()
         {
             InitializeComponent();
+
+            //uint firstInt = 987654321;
+            //var array = IntToBCD5(firstInt);
+            //var outInt = BCD5ToInt(array);
+            //MessageBox.Show(outInt.ToString());
+        }
+
+        public uint BCD5ToInt(byte[] bcd)
+        {
+            uint outInt = 0;
+
+            for (int i = 0; i < bcd.Length; i++)
+            {
+                int mul = (int)Math.Pow(10, (i * 2));
+                outInt += (uint)(((bcd[i] & 0xF)) * mul);
+                mul = (int)Math.Pow(10, (i * 2) + 1);
+                outInt += (uint)(((bcd[i] >> 4)) * mul);
+            }
+
+            return outInt;
+        }
+
+        // Convert an unsigned integer into 5 bytes of 
+        public byte[] IntToBCD5(uint numericvalue, int bytesize = 5)
+        {
+            byte[] bcd = new byte[bytesize];
+            for (int byteNo = 0; byteNo < bytesize; ++byteNo)
+                bcd[byteNo] = 0;
+            for (int digit = 0; digit < bytesize * 2; ++digit)
+            {
+                uint hexpart = numericvalue % 10;
+                bcd[digit / 2] |= (byte)(hexpart << ((digit % 2) * 4));
+                numericvalue /= 10;
+            }
+            return bcd;
         }
 
         private void Connect(object sender, RoutedEventArgs e)
@@ -91,6 +126,19 @@ namespace serialtest
             _serialPort.Write(d, 0, d.Length );
         }
 
+        private void SendHeader(List<byte> b)
+        {
+            b.Add(0xfe);
+            b.Add(0xfe);
+            b.Add(0x64);
+            b.Add(0xe0);
+        }
+
+        private void SendFooter(List<byte> b)
+        {
+            b.Add(0xfd);
+        }
+
         private void SendFreq(object sender, RoutedEventArgs e)
         {
             Console.WriteLine("Send Freq");
@@ -99,18 +147,24 @@ namespace serialtest
 
             List<byte> b = new List<byte>();
 
-            // http://www.plicht.de/ekki/civ/civ-p41.html
-            b.Add(0xfe);
-            b.Add(0xfe);
-            b.Add(0x64);
-            b.Add(0xe0);
+            SendHeader(b);
+
             b.Add(0x05);
-            b.Add(0x30);
-            b.Add(0x21);
-            b.Add(0x80);
-            b.Add(0x14);
-            b.Add(0x00);
-            b.Add(0xfd);
+
+            uint firstInt = 14500000;
+            var array = IntToBCD5(firstInt);
+
+            for (int x = 0; x < array.Length; x++)
+                b.Add(array[x]);
+
+      
+            //b.Add(0x30);
+            //b.Add(0x21);
+            //b.Add(0x80);
+            //b.Add(0x14);
+            //b.Add(0x00);
+
+            SendFooter(b);
 
             byte[] d = b.ToArray();
             _serialPort.Write(d, 0, d.Length);
@@ -120,9 +174,12 @@ namespace serialtest
         {
             while (true)
             {
-                // Console.WriteLine("t");
-                string a = _serialPort.ReadExisting();
-                if ( a != string.Empty) Console.WriteLine(a);
+                if (_serialPort.IsOpen)
+                {
+                    // Console.WriteLine("t");
+                    string a = _serialPort.ReadExisting();
+                    if (a != string.Empty) Console.WriteLine(a);
+                }
                 System.Threading.Thread.Sleep(200);
             }
 
